8fda20d540058bab1229989d8af019ff7a7506fb
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-06 12:33:49 +0200

    changes border animation

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,80 +1,17 @@
-/** @jsx jsx */
-import React, { useEffect, useState, useRef } from 'react';
-import { jsx, css, keyframes } from '@emotion/core';
+import React from 'react';
 
 const CurrentWeather = (props) => {
 
-    const [randNumTopLeft, setRandNumTopLeft] = useState(125);
-    const [randNumTopRight, setRandNumTopRight] = useState(130);
-    const [randNumBottomLeft, setRandNumBottomLeft] = useState(50);
-    const [randNumBottomRight, setRandNumBottomRight] = useState(70);
-  
     // round Temperatur to .5 
     const temp = (props.list[0].main.temp * 2).toFixed() / 2;
 
-    //create Random Numbers for Changes in Border
-    useEffect(() => {
-        numTopLeft();
-        numTopRight();
-        numBottomLeft();
-        numBottomRight();
-    }, [])
-
-    const numTopLeft = () => {
-        return setInterval(() =>
-            setRandNumTopLeft(Math.floor(Math.random() * 40) + 200)
-            , 5000)
-    }
-
-    const numTopRight = () => {
-        return setInterval(() =>
-            setRandNumTopRight(Math.floor(Math.random() * 40) + 150)
-            , 5000)
-    }
-
-    const numBottomLeft = () => {
-        return setInterval(() =>
-            setRandNumBottomLeft(Math.floor(Math.random() * 40) + 130)
-            , 5000)
-    }
-
-    const numBottomRight = () => {
-        return setInterval(() =>
-            setRandNumBottomRight(Math.floor(Math.random() * 40) + 70)
-            , 5000)
-    }
-
-    // store the privious Value for Animation of Border - Changes 
-    function usePrevious(value) {
-        const ref = useRef();
-        useEffect(() => {
-            ref.current = value;
-        }, [value]);
-        return ref.current;
-    }
-    const prevRandNumTopLeft = usePrevious(randNumTopLeft);
-    const prevRandNumTopRight = usePrevious(randNumTopRight);
-    const prevRandNumBottomLeft = usePrevious(randNumBottomLeft);
-    const prevRandNumBottomRight = usePrevious(randNumBottomRight);
-    
-
-
-    // keyframe for Border-Change Animation
-    const border = keyframes`
-    0% {
-        border-radius: ${prevRandNumTopLeft}px ${prevRandNumTopRight}px ${prevRandNumBottomLeft}px ${prevRandNumBottomRight}px;
-    }
-  
-    100% {
-        border-radius: ${randNumTopLeft}px ${randNumTopRight}px ${randNumBottomLeft}px ${randNumBottomRight}px;
-    }
-  `
-
     return (
-        <div css={css`
-        animation: ${border} 5s linear;
-        animation-iteration-count: infinite; 
-      `} className='currentWeather'>
+        <div className='currentWeather'>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
             <div className="pic">
                 <img src={`http://openweathermap.org/img/wn/${props.list[0].weather[0].icon}@2x.png`} />
             </div>
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,5 +1,7 @@
+/** @jsx jsx */
 import React, { useEffect, useState } from 'react';
 import Moment from 'react-moment';
+import { jsx, css, keyframes } from '@emotion/core';
 
 const Day = (props) => {
 
@@ -11,25 +13,46 @@ const Day = (props) => {
     useEffect(() => {
         setrandNum();
     }, [])
-    
+
     const setrandNum = () => {
-        return setInterval(() => setRandNumTopLeft(Math.floor(Math.random() * 40) + 50), Math.floor(Math.random() * 1000)+500)
+        return setInterval(() => setRandNumTopLeft(Math.floor(Math.random() * 40) + 50), 16000)
     }
-    
+
 
     const randNumTopRight = Math.floor(Math.random() * 40) + 30;
     const randNumBottomLeft = Math.floor(Math.random() * 40) + 80;
     const randNumBottomRight = Math.floor(Math.random() * 40) + 50;
+    const randNumTopLeft2 = Math.floor(Math.random() * 40) + 60;
+    const randNumBottomRight2 = Math.floor(Math.random() * 40) + 35;
+    const randNumBottomLeft2 = Math.floor(Math.random() * 40) + 70;
+    const randNumTopRight2 = Math.floor(Math.random() * 40) + 80;
+    const randNumTopLeft3 = Math.floor(Math.random() * 40) + 50;
+    const randNumBottomRight3 = Math.floor(Math.random() * 40) + 30;
+    const randNumBottomLeft3 = Math.floor(Math.random() * 40) + 80;
+    const randNumTopRight3 = Math.floor(Math.random() * 40) + 50;
+    const randNumTopLeft4 = Math.floor(Math.random() * 40) + 60;
+    const randNumBottomRight4 = Math.floor(Math.random() * 40) + 30;
+    const randNumBottomLeft4 = Math.floor(Math.random() * 40) + 80;
+    const randNumTopRight4 = Math.floor(Math.random() * 40) + 50;
 
 
-    const style = {
-        borderRadius: `${randNumTopLeft}px ${randNumTopRight}px ${randNumBottomLeft}px ${randNumBottomRight}px`,
+    // keyframe for Border-Change Animation
+    const morph = keyframes`
+    50% {
+        border-radius: ${randNumTopLeft3}px ${randNumTopRight3}px ${randNumBottomLeft3}px ${randNumBottomRight3}px / ${randNumTopLeft4}px ${randNumTopRight4}px ${randNumBottomLeft4}px ${randNumBottomRight4}px;
     }
-
-
+    25%, 75% {
+        border-radius: ${randNumTopLeft}px ${randNumTopRight}px ${randNumBottomLeft}px ${randNumBottomRight}px / ${randNumTopLeft2}px ${randNumTopRight2}px ${randNumBottomLeft2}px ${randNumBottomRight2}px;
+      }
+    0%, 100% {
+        border-radius: 60% 40% 90% 30% / 60% 60% 70% 90%;
+      }`
 
     return (
-        <div className="Day" style={style}>
+        <div css={css`
+        animation: ${morph} 16s ease-in-out ;
+        animation-iteration-count: infinite; 
+      `} className="Day">
             <Moment format="dddd" className="forcastDay">
                 {props.dt_txt}
             </Moment>
